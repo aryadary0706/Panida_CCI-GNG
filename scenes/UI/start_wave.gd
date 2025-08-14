05cd2b14ac8907a4efa8d1daba4a8ad2adcc4466
Kemas M. Aryadary Rasyad <112590882+aryadary0706@users.noreply.github.com>
2025-08-14 14:53:32 +0700

    wave manage

---
+++ scenes/UI/start_wave.gd
@@ -0,0 +1,7 @@
+extends Panel
+signal wave_started(wave_data: Array)
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var wave_data = [{ "type": "enemy_basic" }, { "type": "enemy_pro" }]
+		emit_signal("wave_started", wave_data)
