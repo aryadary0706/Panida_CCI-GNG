13d35c2ce2951169812e04cf64369f3be10a1a7b
Kemas M. Aryadary Rasyad <112590882+aryadary0706@users.noreply.github.com>
2025-08-29 10:32:43 +0700

    options menu & volume slider

--- Scripts/MainMenu/main_menu.gd
+++ Scripts/MainMenu/main_menu.gd
@@ -1,5 +1,16 @@
 extends Control
 
+@onready var sign_post: Control = $SignPost
+@onready var option: Control = $Option
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	MusicPlayer.play_music(preload("res://audio/MenuGame.ogg"))
+	sign_post.visible = true
+	option.visible = false
+
+
+
+
+func _on_back_pressed() -> void:
+	_ready()
--- Scripts/MainMenu/option_button.gd
+++ Scripts/MainMenu/option_button.gd
@@ -2,4 +2,5 @@ extends TextureButton
 
 func _on_pressed() -> void:
 	#get_tree().change_scene_to_file("res://scenes/Game.tscn")
-	pass
+	get_parent().visible = false
+	get_parent().get_parent().get_node("Option").visible = true
