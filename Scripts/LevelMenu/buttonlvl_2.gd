f3b35790b5c660a87cc50579c8852cac3fb718c5
TheoVol <farrasikhsanulr@gmail.com>
2025-08-29 08:25:35 +0700

    stalls fully animated

--- Scripts/LevelMenu/buttonlvl_2.gd
+++ Scripts/LevelMenu/buttonlvl_2.gd
@@ -1,14 +1,9 @@
 extends Button
 
 @export var level_index: int = 2
-@export var level_scene: String = "res://Level/Lv2/Game.tscn"
-
-func _ready():
-	if level_index > GlobalProgress.unlocked_level:
-		disabled = true
 
 func _on_pressed() -> void:
-	#if not disabled:
-	SfxPlayer.play_music(preload("res://audio/click.ogg"))
-	MusicPlayer.stop_music()
-	get_tree().change_scene_to_file(level_scene)
+	if not disabled:
+		SfxPlayer.play_music(preload("res://audio/click.ogg"))
+		MusicPlayer.stop_music()
+		get_tree().change_scene_to_file("res://Level/Lvl2/Game.tscn" % level_index)
--- Scripts/LevelMenu/level_menu.gd
+++ Scripts/LevelMenu/level_menu.gd
@@ -1,5 +1,17 @@
 extends Control
 
+@onready var level_buttons = [
+	get_node("ButtonLvl_1"),
+	get_node("ButtonLvl_2"),
+	get_node("ButtonLvl_3")
+]
 
 func _ready() -> void:
-	MusicPlayer.play_music(preload("res://audio/MenuGame.ogg"))
+	print_tree_pretty()
+	#MusicPlayer.play_music(preload("res://audio/MenuGame.ogg"))
+	#GlobalProgress.load_progress()
+	#for i in range(level_buttons.size()):
+	#	if i + 1 <= GlobalProgress.unlocked_level:
+	#		level_buttons[i].disabled = false
+	#	else:
+	#		level_buttons[i].disabled = true
