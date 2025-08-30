8f5a2aed0b4e2f5fc76a60d47cd3f5a17fbba94c
Travelling-banana <ezekielherdinata@gmail.com>
2025-08-30 13:39:53 +0800

    Revert "ke main menu"
    This reverts commit 0b60b0b088b374533c16940f8934ad0edf707528.

--- Scripts/UI/game_over_ui.gd
+++ Scripts/UI/game_over_ui.gd
@@ -6,8 +6,7 @@ func _ready() -> void:
 	hide()
 
 func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_file("res://Objects/MainMenu/main_menu.tscn")
-	
+	get_tree().quit()
 
 
 func _on_restart_pressed() -> void:
